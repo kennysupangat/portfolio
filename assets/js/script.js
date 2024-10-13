36c6fa709889f273b59c588cf457ccb485d1db10
Kenny <53990095+kennysupangat@users.noreply.github.com>
2024-10-13 19:11:23 +0900

    update 10/13

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
     $('.menu_box').on('click', function(){
-        alert("The paragraph was clicked.");
-      });
+      $(".menu_display").toggleClass("menu_active");
+      $("html, body").toggleClass("no_scroll");
+      $(".menu_line").toggleClass("line_active");
+    });
+    $('.sp_menu_click').on('click', function(){
+      $(".menu_display").toggleClass("menu_active");
+      $("html, body").toggleClass("no_scroll");
+    });
 });
